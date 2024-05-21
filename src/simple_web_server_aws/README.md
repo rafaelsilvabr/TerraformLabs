33bcc3417e1dd0f3d7b9968c6be2a9f178996ec9
Rafael Silva <r4faelrs@gmail.com>
2024-05-21 11:37:28 -0300

    WebServer configuration by user_data

--- src/simple_web_server_aws/README.md
+++ src/simple_web_server_aws/README.md
@@ -1,2 +1,25 @@
 # Simple Web Server on AWS
 To run this module, you need to have Terraform installed and configured with your AWS credentials. You also need to have an SSH key pair without a passphrase, with the public key located at ~/.ssh/id_rsa.pub on your local machine. This key pair is used for SSH access to the created EC2 instance.  To configure AWS CLI access, you need to install the AWS CLI and run aws configure, then follow the prompts to input your AWS Access Key ID, Secret Access Key, default region name, and default output format.
+
+It's possible to configure the instance in 2 ways, Through an initialization script and through remote access to the instance.  
+To use the initialization script, just use the following setting:
+```
+user_data = file("init.sh")
+```
+
+To use the remote-exec of terraform, just execute the script with the following settings:
+```
+provisioner "remote-exec" {
+  inline = [
+    "sudo yum -y install httpd && sudo systemctl start httpd",
+    "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
+    "sudo mv index.html /var/www/html/"
+  ]
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+}
+```
--- src/simple_web_server_aws/main.tf
+++ src/simple_web_server_aws/main.tf
@@ -6,19 +6,27 @@ resource "aws_instance" "webserver" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum -y install httpd && sudo systemctl start httpd",
-      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
-      "sudo mv index.html /var/www/html/"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
-  }
+  user_data = <<-EOF
+               #!/bin/bash
+               sudo yum -y install httpd
+               sudo systemctl start httpd
+               sudo systemctl enable httpd
+               echo '<h1><center>Olá, WebServer Criado!!</center></h1>' > index.html
+               sudo mv index.html /var/www/html/
+               EOF
+#  provisioner "remote-exec" {
+#    inline = [
+#      "sudo yum -y install httpd && sudo systemctl start httpd",
+#      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
+#      "sudo mv index.html /var/www/html/"
+#    ]
+#    connection {
+#      type        = "ssh"
+#      user        = "ec2-user"
+#      private_key = file("~/.ssh/id_rsa")
+#      host        = self.public_ip
+#    }
+#  }
   tags = {
     Name = "webserver"
   }
